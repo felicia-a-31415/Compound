72854e8f2c3595a613759df575fbfc8f96f59eeb
Lela Star <lelastar8888@gmail.com>
2026-05-30 21:01:35 -0400

    Resolved issue with Supabase package dependency

---
+++ Compound/Services/SupabaseClient.swift
@@ -0,0 +1,14 @@
+//
+//  SupabaseClient.swift
+//  Compound
+//
+//  Created by Felicia on 2026-05-30.
+//
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+    supabaseURL: URL(string: "https://qibiripygptrqrvpysth.supabase.co")!,
+    supabaseKey: "sb_publishable_W8AD9vgzJ4mm8FVocwQJdA_Cxo5Pqgj"
+)
